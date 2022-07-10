e2d15d18842c568edb8acee7908219523c1e0872
Vikash Roy <87648415+vikash03a@users.noreply.github.com>
2022-07-11 00:36:15 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.linkedin.com/in/vikash-kumar03/"
+        style={{ cursor: "pointer" }}
+      >
+        Vikash Kumar
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
